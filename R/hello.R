bf0aea5f01473f425c27f929c4c80d6e861fd05a
Earth-Science Modeling Group <esmg.mx@gmail.com>
2022-08-08 21:06:52 -0500

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,24 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' Title RGeostad2D
+#'
+#'
+#' #' @import Rcpp maps mapproj actuar fields fitdistrplus gstat MASS moments poweRlaw RFOC spatstat ADGofTest reshape sp
+#'
+#' @examples
+hello <- function() {
+  print("Hello, user!")
+}
